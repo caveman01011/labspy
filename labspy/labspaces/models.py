ab90960e1a72d2f7b93d6e3dc079546c50393ac1
caveman01011 <133715324+caveman01011@users.noreply.github.com>
2025-08-17 12:49:33 +0100

    Add Lab and LabMembership models

--- labspy/labspaces/models.py
+++ labspy/labspaces/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from users.models import CustomUser
 
 # Create your models here.
+
+class Lab(models.Model):
+    name = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.name
+
+class LabMembership(models.Model):
+    ROLE_CHOICES = [
+        ('owner', 'Owner'),
+        ('manager', 'Manager'),
+        ('researcher', 'Researcher'),
+        ('guest', 'Guest'),
+    ]
+    lab = models.ForeignKey(Lab, on_delete=models.CASCADE)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    role = models.CharField(max_length=255, choices=ROLE_CHOICES)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"{self.user.username} - {self.lab.name}"
+    
+
