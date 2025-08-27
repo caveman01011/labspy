eb15a6fc12a5cc4e9c809f9cbd284226395687b1
caveman01011 <133715324+caveman01011@users.noreply.github.com>
2025-08-27 14:28:19 +0100

    Fix routes

--- labspy/labspaces/urls.py
+++ labspy/labspaces/urls.py
@@ -4,13 +4,14 @@ from . import views
 app_name = 'labspaces'
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('create_lab/', views.lab_create, name='lab_create'),
-    path('lab/<str:code>/', views.labspace_view, name='labspace_view'),
-    path('lab/<str:code>/pending_requests/', views.pending_requests, name='pending_requests'),
-
     #POST routes
     path('join_lab/', views.lab_join, name="lab_join"),
     path('lab/accept_request/', views.accept_request, name='accept_request'),
     path('lab/reject_request/', views.reject_request, name='reject_request'),
+
+    #Main routes
+    path('', views.home, name='home'),
+    path('create_lab/', views.lab_create, name='lab_create'),
+    path('lab/<str:code>/', views.labspace_view, name='labspace_view'),
+    path('lab/<str:code>/pending_requests/', views.pending_requests, name='pending_requests'),
 ]
